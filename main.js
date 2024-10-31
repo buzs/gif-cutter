9f1ab6faec6a43b81fe38ab46bf2877ee40b540e
Gabriel 'Buz' Henrique <me@gabrielhenrique.net>
2024-10-30 23:10:45 -0300

    update nice

--- main.js
+++ main.js
@@ -1,6 +1,7 @@
 import './style.css'
 
 import { GIFEncoder, quantize, applyPalette } from 'https://unpkg.com/gifenc';
+import 'https://unpkg.com/@zip.js/zip.js@2.7.23/dist/zip.min.js';
 
 document.getElementById('processGIF').addEventListener('click', () => {
   processGIF()
@@ -8,14 +9,14 @@ document.getElementById('processGIF').addEventListener('click', () => {
 document.getElementById('executeRender').addEventListener('click', () => {
   executeRender()
 })
-
+document.getElementById('downloadAll').addEventListener('click', () => {
+  downloadAll()
+})
 
 let generatedGIFs = [];
 let gifs;
-
-let combinedFrames = [];
 const spacing = 10;
-
+const DEBUG = false;
 
 async function processGIF() {
   const file = document.getElementById("gifInput").files[0];
@@ -24,6 +25,7 @@ async function processGIF() {
 
   const arrayBuffer = await file.arrayBuffer();
   const gifData = new Uint8Array(arrayBuffer);
+
   const gif = new GifReader(gifData);
 
   const partWidth = Math.floor(gif.width / columns);
@@ -36,9 +38,11 @@ async function processGIF() {
   }
 
   // Log das dimensões
-  console.log("GIF Original:", gif.width, gif.height);
-  console.log("Partições:", rows, columns);
-  console.log("Dimensões de cada parte:", partWidth, partHeight);
+  if (DEBUG) {
+    console.log("GIF Original:", gif.width, gif.height);
+    console.log("Partições:", rows, columns);
+    console.log("Dimensões de cada parte:", partWidth, partHeight);
+  }
 
   gifs = Array.from({ length: columns * rows }, () =>
     GIFEncoder({
@@ -46,31 +50,24 @@ async function processGIF() {
     })
   );
 
-  combinedFrames = Array.from({ length: gif.numFrames() }, () => []);
-
   // Para cada posição da divisão, criaremos um novo GIF separado
   for (let r = 0; r < rows; r++) {
     for (let c = 0; c < columns; c++) {
       const gifPart = gifs[r * columns + c];
-
-      console.log(gifPart);
 
       // Para cada quadro do GIF original, recorta a parte e adiciona ao novo GIF
       for (let i = 0; i < gif.numFrames(); i++) {
         const frameInfo = gif.frameInfo(i);
         const delay = frameInfo.delay * 10; // Converter para milissegundos
 
-        const frameImageData = new Uint8ClampedArray(
-          gif.width * gif.height * 4
-        );
-        gif.decodeAndBlitFrameRGBA(i, frameImageData);
-
         const frameCanvas = document.createElement("canvas");
         frameCanvas.width = gif.width;
         frameCanvas.height = gif.height;
         const ctx = frameCanvas.getContext("2d");
-        const imgData = new ImageData(frameImageData, gif.width, gif.height);
-        ctx.putImageData(imgData, 0, 0);
+
+        const frameImageData = ctx.getImageData(0, 0, gif.width, gif.height);
+        gif.decodeAndBlitFrameRGBA(i, frameImageData.data);
+        ctx.putImageData(frameImageData, 0, 0);
 
         const partCanvas = document.createElement("canvas");
         partCanvas.width = partWidth;
@@ -83,14 +80,16 @@ async function processGIF() {
           continue;
         }
 
-        // Log das coordenadas de recorte e tamanho do quadro
-        console.log(
-          `Desenhando quadro ${i} na posição [${r},${c}] com coordenadas e tamanho:`,
-          c * partWidth,
-          r * partHeight,
-          partWidth,
-          partHeight
-        );
+        if (DEBUG) {
+          // Log das coordenadas de recorte e tamanho do quadro
+          console.log(
+            `Desenhando quadro ${i} na posição [${r},${c}] com coordenadas e tamanho:`,
+            c * partWidth,
+            r * partHeight,
+            partWidth,
+            partHeight
+          );
+        }
 
         // Desenha a parte específica do quadro, verificando coordenadas válidas
         partCtx.drawImage(
@@ -108,62 +107,32 @@ async function processGIF() {
         partCtx.save();
         partCtx.restore();
 
-        // Adiciona o canvas da parte ao HTML para visualização
-        /*             const container = document.getElementById('gifContainer');
-        const partImage = document.createElement('img');
-        partImage.src = partCanvas.toDataURL(); // Convertendo o canvas para uma imagem
-        container.appendChild(partImage); */
-
         const {
           data,
           width: w,
           height: h,
         } = partCtx.getImageData(0, 0, partWidth, partHeight);
 
-        // If necessary, quantize your colors to a reduced palette
         const palette = quantize(data, 256);
-
-        // console.log("Palette ok");
-
-        // // Apply palette to RGBA data to get an indexed bitmap
         const index = applyPalette(data, palette);
-
-        // Adiciona o quadro recortado ao novo GIF
         gifPart.writeFrame(index, w, h, { delay, palette });
 
-        console.log("Foi");
-
         await new Promise((resolve) => setTimeout(resolve, 0));
-
-        combinedFrames[i][r * columns + c] = partCanvas.toDataURL();
       }
 
       gifPart.finish();
-
       const output = gifPart.bytes();
 
-      console.log("Foi 2", output);
-
-      // const container = document.getElementById("gifContainer");
-      // const partImage = document.createElement("img");
-      // partImage.src = URL.createObjectURL(new Blob([output], { type: 'image/gif' }));
-      // container.appendChild(partImage);
-
-      generatedGIFs.push(URL.createObjectURL(new Blob([output], { type: 'image/gif' })));
-
-      download(output, `part_${r * columns + c}.gif`, { type: 'image/gif' });
+      generatedGIFs.push(new Blob([output], { type: 'image/gif' }));
     }
   }
-}
 
-function download(buf, filename, type) {
-  const blob = buf instanceof Blob ? buf : new Blob([buf], { type });
-  const url = URL.createObjectURL(blob);
-  const anchor = document.createElement("a");
-  anchor.href = url;
-  anchor.download = filename;
-  anchor.click();
-};
+  console.log("GIFs gerados:", generatedGIFs.length);
+
+  // Exibe o botão de download
+  // document.getElementById("downloadButton").style.display = "block";
+  executeRender();
+}
 
 
 async function executeRender() {
@@ -182,7 +151,7 @@ async function executeRender() {
   // Adiciona os GIFs como imagens <img> diretamente no contêiner
   generatedGIFs.forEach((gifData, index) => {
     const imgElement = document.createElement("img");
-    imgElement.src = gifData;
+    imgElement.src = URL.createObjectURL(gifData);
     imgElement.alt = `GIF Part ${index + 1}`;
     imgElement.style.width = "100%"; // Ajusta a largura para caber na coluna
 
@@ -191,15 +160,20 @@ async function executeRender() {
 }
 
 async function downloadAll() {
-  const zip = new JSZip();
+  const zipWriter = new zip.ZipWriter(new zip.BlobWriter("application/zip"));
 
-  generatedGIFs.forEach((blob, index) => {
-    zip.file(`gif_part_${index + 1}.gif`, blob);
+  generatedGIFs.forEach(async (blob, index) => {
+    await zipWriter.add(`part_${index + 1}.gif`, new zip.BlobReader(blob));
   });
 
-  const zipContent = await zip.generateAsync({ type: "blob" });
-  const link = document.createElement("a");
-  link.href = URL.createObjectURL(zipContent);
-  link.download = "gif_parts.zip";
-  link.click();
+  const zipBlob = await zipWriter.close();
+
+  const zipUrl = URL.createObjectURL(zipBlob);
+  const downloadLink = document.createElement("a");
+  downloadLink.href = zipUrl;
+  downloadLink.download = "generated_gifs.zip";
+  downloadLink.click();
+
+  // Limpa o objeto URL para liberar a memória
+  URL.revokeObjectURL(zipUrl);
 }
